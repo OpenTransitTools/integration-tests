35459e260c1181466938e3252d05f0be43901ff5
Frank Purcell <fxpurcell@gmail.com>
2021-02-11 18:49:15 -0800

    fix(url): set test url via env var

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,12 +1,13 @@
 DIR=`dirname $0`
 
+TEST_URL=${TEST_URL:="https://labs.trimet.org"}
 BROWSERS=${BROWSERS:="firefox chrome safari edge"}
 SIDES_DIR=${SIDES_DIR:="./sides"}
 
 cd $DIR/../
 
 for b in $BROWSERS; do
-  sel="selenium-side-runner -c \"browserName=$b\" $SIDES_DIR/*"
+  sel="selenium-side-runner --base-url $TEST_URL -c \"browserName=$b\" $SIDES_DIR/*"
   echo $sel
   eval $sel
 done
