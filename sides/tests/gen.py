5ef38dca276a940a7c88558333c291ca43973258
Frank Purcell <fxpurcell@gmail.com>
2021-08-04 00:14:54 -0700

    fix(suite): work to make python merge individual tests into a suite

---
+++ sides/tests/gen.py
@@ -0,0 +1,19 @@
+"""
+simple utility that generate a bunch of .side files for use in automate.browserstack.com
+
+"""
+import os
+import sys
+from datetime import datetime
+
+this_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.join(this_dir, '..')
+smoke_dir = os.path.join(this_dir, 'smoke')
+
+with open(filename, 'w') as f:
+
+json.load(file object)
+
+
+for rec in browsers:
+     write_cap_file(rec)
