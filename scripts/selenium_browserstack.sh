ee0bc6ae3182029825d879f602031820840d932e
Frank Purcell <fxpurcell@gmail.com>
2021-08-03 23:46:24 -0700

    fix(gen.py + docs): pre-work on parallel test

--- scripts/selenium_browserstack.sh
+++ scripts/selenium_browserstack.sh
@@ -1,14 +1,19 @@
 #
-# run selenium side runner
+# run selenium side runner with Browserstack
+# https://www.selenium.dev/selenium-ide/docs/en/introduction/command-line-runner
+# https://www.browserstack.com/guide/selenium-side-runner
+# https://www.seleniumhq.org/selenium-ide/docs/en/introduction/command-line-runner/#additional-params
 #
 DIR=`dirname $0`
 . $DIR/base.sh
 
 CAPS_FILES="$DIR/browserstack/caps/*cap"
+PARALLEL_PROCESSES=10
 
 for t in $CAPS_FILES; do
     echo $t
-    sel="$SELENIUM_CMD -w 5 \"$SIDES_DIR/*.side\" --parallel-runs --base-url $TEST_URL --config-file $t"
+    # --filter smoke or --filter <regex>
+    sel="$SELENIUM_CMD -w $PARALLEL_PROCESSES --base-url $TEST_URL --config-file $t $SIDES_DIR/*.side"
     echo $sel
-    #eval $sel
+    eval $sel
 done
